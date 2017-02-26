7cf362c13a34664a83b8c6a7a8e5e2db00d3727c
Edy Segura <ednardo@inatel.br>
2017-02-26 18:12:59 -0300

    First commit

---
+++ fizzBuzz-with-intStream/src/main/java/br/edu/java8/intstream/FizzBuzz.java
@@ -0,0 +1,16 @@
+package br.edu.java8.intstream;
+
+public class FizzBuzz {
+	
+	public static void fizzBuzzBeforeJava8(int num) {
+		
+	}
+	
+	public static void fizzBuzzInJava8(int num) {
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println("FizzBuzz game!");
+	}
+}
